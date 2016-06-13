54e06cee258dd77c1c0e3b9ad55f34fbe93fbabb
nicholascarroll <nicholas_matthew_carroll@yahoo.com>
2016-06-13 21:41:09 +1000

    Add files via upload

--- dng2prores.sh
+++ dng2prores.sh
@@ -2,68 +2,78 @@
 # For Blackmagic Production 4K DNG files. Converts to Prores, in the current directory
 # The camera stores the files with this file naming:
 #   Reel/BMPC4KNC_1_2016-05-23_0248_C0000/BMPC4KNC_1_2016-05-23_0248_C0000_######.dng
-# This script assumes they are in a format much like that
+# This script assumes they are in a format like that 
 
 # There is only one command line argument - the directory that the camera creates for the shot.
 # (in next version, this needs to be the parent directory ('reel').
-if [ "$1" == "" ]; then
-    echo
-    echo "$0: Enter the name of the DNG shot directory."
-    echo
+if [ "$1" == "" ]
+then
+    echo "$0: Usage: dng2prores <path to DNG reel directory>. "
+    echo "The Prores clips will be written to you current directory. "
     exit 1
 fi
-if [ ! -d "$1" ]; then
-    echo
-    echo "$0: $1 is not a directory! Enter the name of the directory with the shot in it."
-    echo
+if [ ! -d "$1" ]
+then
+    echo "$0: $1 is not a directory!"
+    echo "Enter the path to the reel."
+    exit 1
+fi
+
+
+CLEAN_ARG1=${1%/*}
+REEL_DIR="$(readlink -f $CLEAN_ARG1)"
+
+TOTAL_SHOTS="$(find $REEL_DIR -iname *.dng -printf "%h\n" | uniq | wc -l)"
+if [ $TOTAL_SHOTS -eq 0 ]
+then
+    echo "No DNG files found there. Exiting"
     exit 1
 fi
 
 # The destination of the copy is implied as the current directory. So first, validate
-# that the current directory has permissions and enough space:
+# that the current directory has permissions:
 
-# permissions
-if [ -w . ]
+if [ ! -w . ]
 then
-    echo
-    echo "Prores clips will be written to " $(pwd)
-    echo
-else
-    echo
     echo "Current directory is not writable. Exiting"
-    echo
-    exit
-fi
-
-SHOT=${1%/}
-
-# free space
-DNG_SIZE="$(du -s $SHOT | awk '{print $1}')"
-FREE_SPACE="$(df . | tail -1 | awk '{print $4}')"
-SPACE_NEEDED="$(($DNG_SIZE * 2))"
-
-if [ $SPACE_NEEDED -gt $FREE_SPACE ]
-then
-    read -p  "Not much space left on disk. Continue anyway? " -n 1 -r
-    echo
-  if [[ ! $REPLY =~ ^[Yy]$ ]]
-  then
     exit 1
-  fi
 fi
 
-# Convert from CinemaDNG to ppm:
-TEMPY="$(mktemp -d)"
-mkdir $TEMPY/"$1"
-ls $SHOT/*.dng | parallel --no-notice --bar "dcraw -c {} > $TEMPY/{.}.ppm"
+# Loop through the reel and process each shot one at a time
+SHOT_COUNTER=1
+for SHOT_DIR in $(find $REEL_DIR -name *.dng -printf "%h\n" | uniq)
+do
+    echo "Processing shot "$SHOT_COUNTER" of "$TOTAL_SHOTS
+    
+    SHOT="$(basename $SHOT_DIR)"
+    # Verify enough free space
+    DNG_SIZE="$(du -s $SHOT_DIR | awk '{print $1}')"
+    FREE_SPACE="$(df . | tail -1 | awk '{print $4}')"
+    SPACE_NEEDED="$(($DNG_SIZE * 2))"
+    
+    if [ $SPACE_NEEDED -gt $FREE_SPACE ]
+    then
+        read -p  "Not much space left on disk. Continue anyway? " -n 1 -r
+        echo
+      if [[ ! $REPLY =~ ^[Yy]$ ]]
+      then
+        exit 1
+      fi
+    fi
+    
+    TEMPY="$(mktemp -d)"    
+    # Convert each dng file to ppm
+    find $SHOT_DIR -iname *.dng | parallel --no-notice --bar "dcraw -h -c {} > $TEMPY/{/.}.ppm" # -h means half size
+    # Build the ppms into a prores proxy file:
+    # -profile flag values:
+    #     0 = Prores Proxy
+    #     1 = Prores LT
+    #     2 = Prores 422
+    #     4 = Prores HQ
+    echo "Writing "$(pwd)/$SHOT.mov
+    ffmpeg -loglevel warning -i $TEMPY/$SHOT"_00%04d.ppm" -c:v prores -profile:v 0 -c:a pcm_s16le ./$SHOT.mov
+    rm $TEMPY/*
+    ((SHOT_COUNTER++))
+done
 
-# Build the ppms into a prores proxy file:
-# -profile flag values:
-#     0 = Prores Proxy
-#     1 = Prores LT
-#     2 = Prores 422
-#     4 = Prores HQ
-ffmpeg -i $TEMPY/$SHOT/$SHOT"_00%04d.ppm" -c:v prores -profile:v 0 -c:a pcm_s16le ./$SHOT.mov
-#rm $TEMPY/$SHOT/*
-
-# Try to do some kind of checksums or at least count the number of frames
+echo "Finished making Prores clips from "$REEL_DIR
